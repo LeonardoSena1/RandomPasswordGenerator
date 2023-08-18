166ed66abdd75da8a5336326c7c2878068f4bc66
Leonardo Sena <leonardo_sena1@hotmail.com>
2023-08-18 10:41:18 -0300

    Add project files.

---
+++ RandomPasswordGenerator/Generator.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Cryptography;
+
+namespace RandomPasswordGenerator
+{
+    public class Generator
+    {
+        private const string lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+        private const string uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string digitChars = "0123456789";
+        private const string specialChars = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+        public static int GetPasswordLength()
+        {
+            Console.Write("Digite o comprimento da senha desejada: ");
+            int length;
+            while (!int.TryParse(Console.ReadLine(), out length) || length <= 0)
+            {
+                Console.Write("Comprimento inválido. Digite um número positivo: ");
+            }
+            return length;
+        }
+
+        public static bool GetYesOrNo(string prompt)
+        {
+            Console.Write(prompt);
+            string input = Console.ReadLine().Trim().ToLower();
+            return input == "y" || input == "yes";
+        }
+
+        public static string GeneratePassword(int length, bool useLowercase, bool useUppercase, bool useDigits, bool useSpecialChars)
+        {
+            string validChars = "";
+            if (useLowercase) validChars += lowercaseChars;
+            if (useUppercase) validChars += uppercaseChars;
+            if (useDigits) validChars += digitChars;
+            if (useSpecialChars) validChars += specialChars;
+
+            if (validChars.Length == 0)
+            {
+                Console.WriteLine("Erro: Nenhum conjunto de caracteres selecionado.");
+                return "";
+            }
+
+            using RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] randomBytes = new byte[length];
+            rng.GetBytes(randomBytes);
+
+            string password = new string(randomBytes.Select(b => validChars[b % validChars.Length]).ToArray());
+            return password;
+        }
+    }
+}
---
+++ RandomPasswordGenerator/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace RandomPasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gerador de Senhas");
+
+            int length = Generator.GetPasswordLength();
+            bool useLowercase = Generator.GetYesOrNo("Incluir letras minúsculas? (y/n): ");
+            bool useUppercase = Generator.GetYesOrNo("Incluir letras maiúsculas? (y/n): ");
+            bool useDigits = Generator.GetYesOrNo("Incluir dígitos? (y/n): ");
+            bool useSpecialChars = Generator.GetYesOrNo("Incluir caracteres especiais? (y/n): ");
+
+            string generatedPassword = Generator.GeneratePassword(length, useLowercase, useUppercase, useDigits, useSpecialChars);
+            Console.WriteLine($"Senha gerada: {generatedPassword}");
+        }
+    }
+}
